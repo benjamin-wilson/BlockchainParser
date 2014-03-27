fc4d0b95cce6782fbfdaff63a218e971b31e5b48
tiny1600 <dgkincaid16@gmail.com>
2014-03-27 11:48:11 -0400

    Added graph project

---
+++ Parser/GraphDataStructure/GraphNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDataStructure
+{
+    public class GraphNode<T>
+    {
+        private T data;
+
+        private List<int> costs;
+
+        public GraphNode() { }
+        public GraphNode(T data) : this(data, null) { }
+        public GraphNode(T value, NodeList<T> neighbors) : base(value, neighbors) { }
+    }
+}
