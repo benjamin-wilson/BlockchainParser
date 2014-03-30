abc6ce0661536a455ee209d61556ac45715d8dad
douglas.kincaid <douglas.kincaid@mymail.champlain.edu>
2014-03-29 23:06:50 -0400

    moved the functions in Run project to graph project

--- Parser/GraphDataStructure/Graph.cs
+++ Parser/GraphDataStructure/Graph.cs
@@ -140,5 +140,46 @@ namespace GraphDataStructure
                 }
             }
         }
+
+        public static Graph populate(string publicAddress)
+        {
+            int count = 0; 
+            Queue<string> nextAddresses = new Queue<string>();
+            GraphDataStructure.Graph graphList = new GraphDataStructure.Graph();
+
+            graphList.addGraphNode(publicAddress);
+            nextAddresses.Enqueue(publicAddress);
+
+            while (nextAddresses.Count > 0)
+            {
+                Database.DBConnect getLists = new Database.DBConnect();
+                string current = nextAddresses.Dequeue();
+
+                var sendersList = getLists.getSentTo(current);
+                var reciverList = getLists.getRecivedFrom(current);
+
+                foreach (var sender in sendersList)
+                {
+                    nextAddresses.Enqueue(sender.target);
+                    graphList.addGraphNode(sender.target);
+
+                    graphList.addDirectedEdge(sender.source, sender.target);
+                    // Console.WriteLine(sender.source + "     " + sender.target);
+                }
+
+                foreach (var reciver in reciverList)
+                {
+                    nextAddresses.Enqueue(reciver.source);
+                    graphList.addGraphNode(reciver.source);
+
+                    graphList.addDirectedEdge(reciver.source, reciver.target);
+                    //  Console.WriteLine(reciver.source + "    " + reciver.target);
+                }
+
+                count++;
+            }
+
+            return graphList;
+        }
     }
 }
